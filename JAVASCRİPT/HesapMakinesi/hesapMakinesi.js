809c7d731bddb2dabb0ddcbb41bc5ea4f0f5dd3e
ErgunSekercan <ergunsekercan@gmail.com>
2024-08-13 17:08:53 +0300

    ilk

---
+++ JAVASCRİPT/HesapMakinesi/hesapMakinesi.js
@@ -0,0 +1,16 @@
+document.querySelector(".calculator").addEventListener("click", (e) =>{
+
+    // console.log(e.target.innerText); 
+
+    const btnValue = e.target.value
+    let displayNewValue = document.querySelector(".display").innerHTML += btnValue
+    
+        
+        
+})
+
+const multiplication = (a,b) =>{
+    return(a*b)
+}
+
+ 
